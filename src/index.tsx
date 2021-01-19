53717ba38360a05581070f3ddee77f3903cca6a7
Marcus Grant <marcusfg@gmail.com>
2021-01-19 22:56:37 +0100

    feat: add rebass & styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Button } from 'rebass';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Button></Button>
   </React.StrictMode>,
   document.getElementById('root')
 );
