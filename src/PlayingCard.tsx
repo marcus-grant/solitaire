5e124b7ef9833560ee5476360245b6565c635269
Marcus Grant <marcusfg@gmail.com>
2021-03-10 21:42:43 +0100

    feat: add PlayingCard.tsx to rep card components

---
+++ src/PlayingCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const CARD_SUITS = [
+ 'SPADE',
+ 'CLUBS',
+ 'DIAMOND',
+ 'HEART',
+ 'CIRCLE',
+ 'BAMBOO',
+ 'CHARS',
+ 'DRAGON',
+];
+
+export type CardSuit = 
+
+type CardRank =
+| 1
+| 2
+| 3
+| 4
+| 5
+| 6
+| 7
+| 8
+| 9
+| 'JACK'
+| 'QUEEN'
+| 'KING'
+| 'ACE'
+| ''
+
+
+export const PlayingCard = ()
